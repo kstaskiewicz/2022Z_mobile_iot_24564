c6a6d2c6077e15360ddad909271d6528b58148f9
kstaskiewicz <kstaskiewicz@edu.cdv.pl>
2022-10-16 08:14:24 +0200

    Lab01

---
+++ Lab01/Lab01/obj/Debug/net6.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("Lab01.MainApplication, Lab01, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc64754992eb83202837.MainApplication.class, crc64754992eb83202837.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
