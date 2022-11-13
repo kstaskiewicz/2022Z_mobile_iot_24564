a007bfeb5ab0236b83a52e864184e7fcaa8e1b71
kstaskiewicz <kstaskiewicz@edu.cdv.pl>
2022-11-13 09:16:31 +0100

    Lab02

---
+++ Lab02/Lab02/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab02;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Lab02/Lab02/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab02;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
